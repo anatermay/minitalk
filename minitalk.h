ad5205f706fbd373c694a777ca99270ce67bf6be
Ana Ternero Mayorga <anaternero98@gmail.com>
2024-11-24 18:53:55 +0100

    IN PROGRESS: _Minitalk_UNIX_Signals_ .
    ✅ MINITALK....
        All ok?
        .
        .
        .
             🔃
                      ✔️ Programme structure
    
    ✅ BONUS: CHECKER...
        All ok?
        .
        .
        .
             🔃
                      ✔️ Programme structure
    
     _ _ _
    
    📅 Last updated: Sun 11/24/2024 18:53
    💻login: aternero
     . . . 🏫 42 Malaga
    📷 social_media: @anatermay
    .
    .
    .
    🖤✨

---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aternero <aternero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 18:26:25 by aternero          #+#    #+#             */
+/*   Updated: 2024/11/24 18:31:39 by aternero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+/*FUNCTIONS*/
+
+#endif
---
+++ minitalk_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aternero <aternero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 18:26:25 by aternero          #+#    #+#             */
+/*   Updated: 2024/11/24 18:31:52 by aternero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+/*FUNCTIONS*/
+
+#endif
